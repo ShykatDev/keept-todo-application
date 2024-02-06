3cb39331bc87f5ea177232aa60532c13b144ec51
Shykat Raha <97785935+ShykatDev@users.noreply.github.com>
2024-02-07 02:06:25 +0600

    Update DeletePopup.jsx

--- src/components/DeletePopup.jsx
+++ src/components/DeletePopup.jsx
@@ -1,7 +1,7 @@
 import { FaTrash } from "react-icons/fa6";
 const DeletePopup = ({ confirmDelete, onClose }) => {
   return (
-    <div className="bg-white dark:bg-black dark:bg-opacity-50 absolute min-w-[22rem] rounded-md bottom-10 right-10 p-4 text-neutral-700 dark:text-neutral-500 border border-neutral-400 dark:border-neutral-700 z-50">
+    <div className="bg-white dark:bg-neutral-900 absolute min-w-[22rem] rounded-md bottom-10 right-10 p-4 text-neutral-700 dark:text-neutral-500 border border-neutral-400 dark:border-neutral-700 z-50">
       <p className="flex items-center gap-2">
         <FaTrash className="text-red-500" /> Do you really want to delete?
       </p>
